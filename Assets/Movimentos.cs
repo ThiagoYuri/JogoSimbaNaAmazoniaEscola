cfcafda5d851642c7d2a6194f2f16af0d39fd8a7
Monteiro <thiagocabelin1234@gmail.com>
2021-02-10 08:26:40 -0300

    Versão 1

---
+++ Assets/Movimentos.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentos : MonoBehaviour {
+
+
+
+    public Rigidbody2D perso;
+
+    public float pulo;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, pulo);
+
+        }
+      //      Rigidbody2D.AddForce(Vector2.Up * 200);
+       // }
+
+
+	}
+
+
+
+}
---
+++ Assets/script/MovimentoMapa.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoMapa : MonoBehaviour {
+
+
+    int numeroBack = 15;
+        // Use this for initialization
+        void Start () {
+  
+        }
+    
+    
+    // Update is called once per frame
+        void Update () { 
+        transform.position += new Vector3(-3 * Time.deltaTime, 0, 0);
+        }
+
+    void OnTriggerEnter2D(Collider2D outro) {
+
+        if(outro.transform.tag == "backmover") {
+            float larguraBack = GetComponent<BoxCollider2D>().size.x;
+
+            Vector3 posicao = GetComponent<BoxCollider2D>().transform.position;
+
+            posicao.x += larguraBack * numeroBack;
+
+            GetComponent<BoxCollider2D>().transform.position = posicao;
+        }
+
+    }
+
+            
+  }
+
